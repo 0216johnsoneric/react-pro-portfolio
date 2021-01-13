451ec296885c9a902e543501200c42154fce4db8
Eric Johnson <ericjohnson0216@icloud.com>
2021-01-12 20:46:09 -0500

    Done.

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -8,13 +8,13 @@ const Hero = () => {
 
         <div className="bigimg w3-display-container justify-text-center" id="home">
             
-                <div className="heroContent">
+                 <div className="heroH1"> 
 
-                    <span className="heroH1"> Eric Johnson</span>
+                    <span className="w3-hide-small"> Eric Johnson</span>
                     <br/>
                     <span className="w3-hide-small ">Software Engineering and Development</span>
-                </div>
-                
+                 </div> 
+            
             
         </div>
         </div>
