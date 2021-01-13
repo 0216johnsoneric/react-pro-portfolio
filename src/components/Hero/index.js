1881ee3a193a608e5bd40d8e94485c9292ed9590
Eric Johnson <ericjohnson0216@icloud.com>
2021-01-12 19:30:22 -0500

    Done.

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -4,14 +4,19 @@ import "./style.css";
 const Hero = () => {
     return (
         <>
+        <div className="containerBG">
+
         <div className="bigimg w3-display-container justify-text-center" id="home">
             
-                
-                    <span className="w3-hide-small"> Eric Johnson</span>
+                <div className="heroContent">
+
+                    <span className="heroH1"> Eric Johnson</span>
                     <br/>
-                    <span className="w3-hide-small">Software Engineering and Development</span>
+                    <span className="w3-hide-small ">Software Engineering and Development</span>
+                </div>
                 
             
+        </div>
         </div>
         </>
     )
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,16 +1,16 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 
 const Nav = () => {
     return (
         <>
     
-            <div className="w3-top">
+            <div className="w3-top zeed">
                 <div className="w3-sidebar" id="myNavbar">
                     <a className="w3-bar-item button w3-hover-black w3-hide-medium w3-hide-large w3-right" href="javascript:void(0);" onclick="toggleFunction()" title="Toggle Navigation Menu"> <i className="fa fa-bars"></i></a>
                     <br/><br/>
-                    <a href="#home" className="w3-bar-item button w3-hide-small"> <i className="fa fa-home fa-2x"></i>&emsp;</a>
-                    <a href="#portfolio" className="w3-bar-item button w3-hide-small"> <i className="fa fa-th fa-2x"></i>&emsp;</a>
+                    <Link to="/"><a href="#home" className="w3-bar-item button w3-hide-small"> <i className="fa fa-home fa-2x"></i>&emsp;</a></Link>
+                    <Link to="/portfolio"><a href="#portfolio" className="w3-bar-item button w3-hide-small"> <i className="fa fa-th fa-2x"></i>&emsp;</a></Link>
                     <a href="#about" className="w3-bar-item button w3-hide-small"> <i className="fa fa-user fa-2x"></i></a>
                     <a href="#contact" className="w3-bar-item button w3-hide-small"> <i className="fa fa-envelope fa-2x"></i></a>
                 </div>
