659c090b6e9ecfa1aea095830fab65aa9d7abe66
Eric Johnson <ericjohnson0216@icloud.com>
2021-01-18 17:34:09 -0500

    Done.

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,35 +3,46 @@ import React from 'react'
 
     const Contact = () => {
         return (
-        <>
+        
+            <>
         <div class="w3-display-container">
             <div class="w3-display-middle">
-                <span class="w3-xxlarge w3-text-white w3-wide">CONTACT</span>
+                    <span class="w3-xxlarge w3-text-white w3-wide">CONTACT</span>
             </div>
         </div>
 
-         <div class="w3-content w3-container w3-padding-64" id="contact">
-            <h2 class="w3-center"><em>LOCATION</em></h2>
-         <div class="w3-row w3-padding-32 w3-section">
-            
-            <div class="w3-panel w3-center">
-                <div class="w3-large w3-margin-bottom">
-                    <i class="fa fa-map-marker fa-fw w3-hover-text-black w3-xlarge w3-margin-right">Connecticut, USA</i> 
-                    <br></br>
-                    <i class="fa fa-phone fa-fw w3-hover-text-black w3-xlarge w3-margin-right">Phone: (860) 387-5835</i> 
-                    <br></br>
-                    <i class="fa fa-envelope fa-fw w3-hover-text-black w3-xlarge w3-margin-right">Email: ericjohnson0216@icloud.com</i> 
-                    <br></br>
-                </div>
-               
-                
-                
-            </div>
-           
-        </div>
-    </div>
 
-    </>
+        <form className="form">
+          <input
+            value={this.state.email}
+            name="Name"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="Email"
+          />
+          <input
+            value={this.state.username}
+            name="Email"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="Username"
+          />
+          <input
+            value={this.state.password}
+            name="Message"
+            onChange={this.handleInputChange}
+            type="password"
+            placeholder="Password"
+          />
+          <br></br>
+          <button className="btn btn-outline-light bg-dark" onClick={this.handleLogin}>Enter</button>
+        </form>
+      
+        
+        </>
+         
+    
+    
         )
     }
 
